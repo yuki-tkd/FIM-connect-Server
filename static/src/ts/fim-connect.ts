1823832a1f233a2d6aa6c6c15318a8a877fa64c4
Yuki Takeda <yuki@imaginaryshort.com>
2017-05-09 15:36:52 +0900

    u

--- static/src/ts/fim-connect.ts
+++ static/src/ts/fim-connect.ts
@@ -3,6 +3,7 @@ import * as Index from "./view/index";
 
 Router.connect('Index', Index.init);
 
+
 function dispatch() {
     const html: Element = document.documentElement;
     const scope: string = html.getAttribute('data-page-scope');
